120ce5fec1f08cfa9983eadcb2e222a182932c27
shubh gupta <sept1st2c@gmail.com>
2025-02-01 12:57:44 +0530

    heh

---
+++ yamlize/src/app/contact/page.jsx
@@ -0,0 +1,8 @@
+// pages/contact.jsx
+import React from "react";
+
+const ContactPage = () => {
+  return <h1>Contact Us</h1>;
+};
+
+export default ContactPage;
---
+++ yamlize/src/app/main/page.jsx
@@ -0,0 +1,8 @@
+// pages/index.jsx
+import React from "react";
+
+const HomePage = () => {
+  return <h1>Welcome to My Next.js Website</h1>;
+};
+
+export default HomePage;
