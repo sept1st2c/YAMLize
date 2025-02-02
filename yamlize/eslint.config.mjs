416276d318a3d97ab468ba7444a0b749cbc4120c
shubh gupta <sept1st2c@gmail.com>
2025-02-02 07:16:22 +0530

    he

--- yamlize/eslint.config.mjs
+++ yamlize/eslint.config.mjs
@@ -1,11 +1,21 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const eslintConfig = {
-  extends: ["next/core-web-vitals"],
-};
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = compat.config({
+  overrides: [
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      extends: ["next/core-web-vitals"],
+    },
+  ],
+});
 
 export default eslintConfig;
